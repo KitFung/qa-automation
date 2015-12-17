ac3291523750496990d05577338e456bb470d09d
Kit Fung <sa9510@gmail.com>
2015-12-17 12:50:41 +0800

    Add the logging message for the TestTutorialPage and TestWelcomePage

--- Robotium/bindle/BindleAutomationTest/src/com/oursky/bindle/TestTutorialPage.java
+++ Robotium/bindle/BindleAutomationTest/src/com/oursky/bindle/TestTutorialPage.java
@@ -2,6 +2,7 @@ package com.oursky.bindle;
 
 import android.test.suitebuilder.annotation.SmallTest;
 import android.test.suitebuilder.annotation.Smoke;
+import android.util.Log;
 import android.widget.TextView;
 
 
@@ -18,23 +19,35 @@ public class TestTutorialPage extends AndroidTestBase {
 	
 	@Smoke
     public void testTutorialPageExist() {
+		Log.d(TAG, ">>> Test: testTutorialPageExist   -- Start -- <<<");
+		Log.d(TAG, "check tutorial page exist");
     	device().waitForActivity("TutorialActivity");
         assertTrue("Tutorial Page haven't display", device().searchText("Welcome"));
+        Log.d(TAG, "checked tutorial page exist");
+        Log.d(TAG, ">>> Test: testTutorialPageExist   -- End -- <<<");
     }
     
 	@SmallTest
     public void testNextPageOfTutorialPage() {
+		Log.d(TAG, ">>> Test: testNextPageOfTutorialPage   -- Start -- <<<");
+		
+		Log.d(TAG, "load tutorial page");
     	device().waitForActivity("TutorialActivity");
-    	
     	TextView NextBtn = (TextView) device().getView("action_done");
-        device().clickOnView(NextBtn);
-        
+    	Log.d(TAG, "loaded tutorial page");
+
+    	Log.d(TAG, "Go to the next page of the tutorial page");
+    	device().clickOnView(NextBtn);
         device().waitForActivity("WelcomeActivity");
-        
+        Log.d(TAG, "Successfully go to the next page of the tutorial page");
+
+        Log.d(TAG, "Checking the welcome page component");
         device().assertCurrentActivity("Next Page of tutorial page is welcome page", "WelcomeActivity");
-        
         assertTrue("Welcome Page don't have sign up button.", device().searchButton("Sign up"));
         assertTrue("Welcome Page don't have log in button.", device().searchButton("Log in"));
+        Log.d(TAG, "Checked the welcome page component");
+
+        Log.d(TAG, ">>> Test: testNextPageOfTutorialPage   -- End -- <<<");
 	}
     
 }
--- Robotium/bindle/BindleAutomationTest/src/com/oursky/bindle/TestWelcomePage.java
+++ Robotium/bindle/BindleAutomationTest/src/com/oursky/bindle/TestWelcomePage.java
@@ -12,25 +12,40 @@ public class TestWelcomePage extends AndroidTestBase{
 
 	@Smoke
 	public void testSignUpBtn() {
+		Log.d(TAG, ">>> Test: testSignUpBtn   -- Start -- <<<");
+
+		Log.d(TAG, "Searching the signup button");
     	device().waitForActivity("WelcomeActivity");
         assertTrue("Sign up button not exist", device().searchButton("Sign up"));
+        Log.d(TAG, "Found the signup button");
         
+        Log.d(TAG, "Checking the redirect of the signup button");
         device().clickOnButton("Sign up");
         device().waitForActivity("SignUpActivity");
         device().assertCurrentActivity("The Sign Up Button haven't redirect the user to sign up page",
         		"SignUpActivity");
+        Log.d(TAG, "Checked the redirect of the signup button");
+        
+        Log.d(TAG, ">>> Test: testSignUpBtn   -- End -- <<<");
 	}
 	 
 	@Smoke
 	public void testLoginBtn() {
+		Log.d(TAG, ">>> Test: testLoginBtn   -- Start -- <<<");
+		
+		Log.d(TAG, "Searching the login button");
     	device().waitForActivity("WelcomeActivity");
         assertTrue("Log in button exist", device().searchButton("Log in"));
-    
+        Log.d(TAG, "Found the login button");
+        
+        Log.d(TAG, "Checking the redirect of the login button");
         device().clickOnButton("Log in");
         device().waitForActivity("LoginActivity");
         Log.i("1200120", device().getCurrentActivity().toString());
         device().assertCurrentActivity("The Log in Button haven't redirect the user to log in page",
         		"LoginActivity");
-        
+        Log.d(TAG, "Checked the redirect of the login button");
+
+        Log.d(TAG, ">>> Test: testLoginBtn   -- End -- <<<");
 	}
 }
